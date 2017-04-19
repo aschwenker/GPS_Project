f976fe64680bbd22aed68f34992a083892147c26
aschwenker <anne.schwenker@gmail.com>
2017-04-19 13:26:39 -0400

    added Lerone function, return geo failed

--- point_to_mutli_line_redo.py
+++ point_to_mutli_line_redo.py
@@ -7,7 +7,7 @@ import csv
 import shutil
 import gzip
 import math
-
+from collections import deque, Counter
 config = {
   'global' : {
     'server.socket_host' : '127.0.0.1',
@@ -15,7 +15,6 @@ config = {
     'server.thread_pool' : 8,
   }
 }
-
 
 
 
@@ -27,6 +26,7 @@ def haversine_np(x):
     All args must be of equal length.    
 
     """
+
     lon1 = float(x[0][0])
     lat1 = float(x[0][1])
     lon2 = float(x[1][0])
@@ -42,9 +42,9 @@ def haversine_np(x):
     km = 6367 * c
     return km
     
-def gpsroute(file_content):
-    uploadlist=[]
-    uploadlist = file_content.split('\n') 
+def gpsroute(uploadlist):
+    #uploadlist=[]
+    #uploadlist = file_content.split('\n') 
     results ={"type":"FeatureCollection","features":[]}
     x={}
     featurelist = []
@@ -61,28 +61,48 @@ def gpsroute(file_content):
     lonlatlist =sorted(lonlatlist, key=lambda x: x[2])
     for listlon in lonlatlist:
         latlon=((listlon[0],listlon[1]))
-        latlonlist.append(latlon)
+        time = float(listlon[2])
+        appenditem=(latlon,time)
+        latlonlist.append(appenditem)
     
     lonLatLength = len(latlonlist)
+    print lonLatLength
     for i in range(1,lonLatLength-1):
-        valuedict = {"type": "Feature","geometry":{"type" : "LineString","coordinates":[]},"properties":{"distance":""} }
+        valuedict = {"type": "Feature","geometry":{"type" : "LineString","coordinates":[]},"properties":{"distance":"","mean_speed":"","duration":""} }
         a = i
         b = i + 1
-        x=latlonlist[a],latlonlist[b]
-        print x
-        latinput =x[0][0]
-        print latinput
-        print type(latinput)
-        longlatinput = float(latinput)
-        print type(longlatinput)
+        x=latlonlist[a][0],latlonlist[b][0]
         distance = haversine_np(x)
-        print distance
+        time = float(latlonlist[b][1])-float(latlonlist[a][1])
+        if time > 0:
+            speed = float(distance)/float(time)
+            z=(x,speed)
+        else:
+            continue
+        if i >3:
+            meanseqstart =i-2
+            print meanseqstart
+            meanseq = meanseqstart+4
+            piece = latlonlist[meanseqstart:meanseq]
+            seq=[]
+            for i in range (0,len(piece)):
+                one = piece[i][1]
+                print one
+                seq.append(one)
+            print seq
+            seqSize = len(seq)
+            d=deque(seq[0:seqSize])
+            mean_speed = [np.mean(d)]
+        else:
+            mean_speed = speed
+
         valuedict["geometry"]["coordinates"] =x
         valuedict["properties"]["distance"] = distance
+        valuedict["properties"]["mean_speed"] = mean_speed
+        valuedict["properties"]["duration"] = time
         featurelist.append(valuedict)
     results["features"] = featurelist
-    geojson = json.dumps(results)
-    return geojson
+    return (json.dumps(results))
 
         
     
@@ -91,21 +111,62 @@ class App:
     def index(self):
         return file('C:/Users/ASchwenker/Documents/GitHub/GPS_Project/index.html')
     @cherrypy.expose
-    def upload(self):
-        '''Handle non-multipart upload'''
-        upload = cherrypy.request.body.read()
-        return upload
-    
+    def geo(self):
+        return file('C:/Users/ASchwenker/Documents/GitHub/GPS_Project/apriltest.geojson')
     @cherrypy.expose
-    def geojson(self):
-        return gpsroute(self.upload())
+    @cherrypy.tools.json_out()
+    def upload(self):
+        upload = cherrypy.request.body.read()
+        print type(upload)
+        uploadlist=[]
+        uploadlist = upload.split('\n')
 
-        #with gzip.open(upload,'r') as f:
-        #    file_content=f.read()
-        #print type(file_content)
-        #with open(destination, 'wb') as f:
-        #    shutil.copyfileobj(cherrypy.request.body, f)
-            
+        return gpsroute(uploadlist)
+
+    
+    """@cherrypy.expose
+    def gpsroute(self):
+        upload = cherrypy.request.body.read()
+        #return upload
+        #upload = self.upload()
+        print upload
+        uploadlist=[]
+        uploadlist = upload.split('\n')
+        
+        results ={"type":"FeatureCollection","features":[]}
+        x={}
+        featurelist = []
+        lonlatlist = []
+        latlonlist = [] 
+        xlist=[x for x in range(1,len(uploadlist))]
+        for x in xlist:
+            row =uploadlist[x]
+            row = row.split(",")
+            #print row
+            #print type(row)
+            lonlatlist.append([row[1],row[2],row[25]])
+            #print lonlatlist
+        lonlatlist =sorted(lonlatlist, key=lambda x: x[2])
+        for listlon in lonlatlist:
+            latlon=((listlon[0],listlon[1]))
+            latlonlist.append(latlon)
+        
+        lonLatLength = len(latlonlist)
+        for i in range(1,lonLatLength-1):
+            valuedict = {"type": "Feature","geometry":{"type" : "LineString","coordinates":[]},"properties":{"distance":""} }
+            a = i
+            b = i + 1
+            x=latlonlist[a],latlonlist[b]
+            latinput =x[0][0]
+            longlatinput = float(latinput)
+            distance = haversine_np(x)
+            valuedict["geometry"]["coordinates"] =x
+            valuedict["properties"]["distance"] = distance
+            featurelist.append(valuedict)
+        results["features"] = featurelist
+        geojson = json.dumps(results)
+        print geojson"""
+        
 
     
         
