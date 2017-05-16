43ef8ec12035d5aacdb04c6b4b1ed2143e88c3c4
aschwenker <anne.schwenker@gmail.com>
2017-05-16 10:22:12 -0400

    separated core and supporting functions

---
+++ Core.py
@@ -0,0 +1,57 @@
+
+import os
+import cherrypy
+import shutil
+import tempfile
+import uuid
+from Modules import gpsroute
+
+config = {
+  'global' : {
+    'server.socket_host' : '127.0.0.1',
+    'server.socket_port' : 8080,
+    'server.thread_pool' : 8,
+    'server.environment' : "production",
+    'engine.autoreload_on' : True,
+    'engine.autoreload_frequency' : 60
+  }
+}
+
+def writer(csvdestination):
+    with open(csvdestination, 'wb') as f:
+        shutil.copyfileobj(cherrypy.request.body, f)
+
+class App:
+    directory_name = tempfile.mkdtemp()
+    csvfilename    = str(uuid.uuid4())
+    jsonfilename = str(uuid.uuid4())
+    jsondestination = os.path.join(directory_name, jsonfilename)
+    csvdestination = os.path.join(directory_name, csvfilename)
+    @cherrypy.expose
+    def index(self):
+        """loads the index file"""
+        return file('C:/Users/ASchwenker/Documents/GitHub/GPS_Project/index.html')
+        
+    @cherrypy.expose
+    def upload(self):
+        """uploads csv from request,wrtites a temporary csv to random string name"""
+        writer(self.csvdestination)
+
+    @cherrypy.expose
+    def userinput(self,unique):    
+        """user provides unique string to access their db data"""
+    @cherrypy.expose
+    def geo(self):
+        gpsroute(self.csvdestination,self.jsondestination)
+        """used as a static geojson sample to test html geojson link load worked"""
+        return file(self.jsondestination)
+
+        
+
+if __name__ == '__main__':
+  cherrypy.quickstart(App(), '/', config)
+
+        
+
+
+
---
+++ Modules.py
@@ -0,0 +1,109 @@
+import json
+import numpy as np
+from datetime import datetime
+import csv
+import os
+
+def isTimeFormat(input):
+    """allows us to determine the date format for further processes"""
+    try:
+        datetime.strptime(input, '%m/%d/%Y %H:%M:%S')
+        return True
+    except ValueError:
+        return False
+
+
+def haversine_np(x):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.    
+
+    """
+
+    lon1 = float(x[0][0])
+    lat1 = float(x[0][1])
+    lon2 = float(x[1][0])
+    lat2 = float(x[1][1])
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+    return km
+    
+    
+def gpsroute(csvdestination,jsondestination):
+    """Takes in the gps data as a list, reads it and enriches it.
+    then returns a geojson feature collection of line segments between each lat,lon given"""
+
+    results ={"type":"FeatureCollection","features":[]}
+    x={}
+    featurelist = []
+    lonlatlist = []
+    latlonlist = []
+    timeheader = ''
+    with open(csvdestination,'r') as csvfile:
+        headerreader = csv.reader(csvfile)
+        headers = headerreader.next()
+    csvfile.close()
+    for header in headers:
+        if 'time' in header.lower():
+            timeheader = header
+        if 'lat' in header.lower():
+            latheader = header
+        if 'lon' in header.lower():
+            lonheader = header
+    with open(csvdestination,'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            timeraw=row[timeheader]
+            print timeraw
+            if isTimeFormat(timeraw):       
+                time = datetime.strptime(timeraw,"%m/%d/%Y %H:%M:%S")
+                print time
+                lonlatlist.append([[row[lonheader],row[latheader]],time])
+            else:
+                time = datetime.strptime(timeraw,"%m/%d/%Y %H:%M")
+                lonlatlist.append([[row[lonheader],row[latheader]],time])
+            #appends to a list to the list: [[lat, lon ],time]
+            #necessary becuase couldnt perform following sort if it were a tuple
+    lonlatlist =sorted(lonlatlist, key=lambda x: x[1])
+    #sorts on time
+    for listlon in lonlatlist:
+        latlon=((float(listlon[0][0]),float(listlon[0][1])))
+        #tuple
+        time = listlon[1]
+        appenditem=([latlon,time])
+        #restructuring format
+        latlonlist.append(appenditem)
+        #list of ((lat,lon),time)
+    
+    for i in range(0,len(latlonlist)-1):
+        #must be range(len-1) to accomodate line segment lat/lon pairs
+        valuedict = {"type": "Feature","geometry":{"type" : "LineString","coordinates":[]},"properties":{"distance":"","speed":"","duration":""} }
+        #creates dictionary that will populate feature list of dict created above
+        x=latlonlist[i][0],latlonlist[i+1][0]
+        #((lon,lat),(lon,lat)) (tuple,tuple) represents start & end of line seg
+        distance = round(haversine_np(x),2)
+        time = latlonlist[i][1]-latlonlist[i+1][1]
+        #start and end time for seg
+        duration = (time.seconds/60)
+        if duration > 0:
+            speed = round((float(distance)/float(duration)),2)
+        else:
+            speed = 0
+        x=latlonlist[i][0],latlonlist[i+1][0]
+        valuedict["geometry"]["coordinates"] =x
+        valuedict["properties"]["distance"] = distance
+        valuedict["properties"]["speed"] = speed
+        valuedict["properties"]["duration"] = duration
+        featurelist.append(valuedict)
+    results["features"] = featurelist
+    with open (jsondestination,'w')as outfile:
+        json.dump(results,outfile)
